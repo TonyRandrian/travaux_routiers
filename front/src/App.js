e73c3bee4ab06127d73d0df76affb7f373504bf3
RANDRIANARISON Tony <157312069+TonyRandrian@users.noreply.github.com>
2026-01-13 21:58:44 +0300

    add db connection

--- front/src/App.js
+++ front/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [message, setMessage] = useState('');
+  const [dbMessage, setDbMessage] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:3000/')
@@ -11,12 +12,21 @@ function App() {
       .catch(error => setMessage('Erreur de connexion'));
   }, []);
 
+  const testDB = () => {
+    fetch('http://localhost:3000/db')
+      .then(response => response.json())
+      .then(data => setDbMessage(JSON.stringify(data)))
+      .catch(error => setDbMessage('Erreur DB'));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Travaux Routiers Front</h1>
         <p>Welcome to the front-end!</p>
         <p>Message du back: {message}</p>
+        <button onClick={testDB}>Tester DB</button>
+        <p>{dbMessage}</p>
       </header>
     </div>
   );
